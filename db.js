2ded41e67548a7aad03a9cd007b8ae54a89ba1c8
chanpi23 <chandrikad34@gmail.com>
2022-08-08 19:18:50 -0400

    update gitignore

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
-const mongoose= require ('mongoose')
+const mongoose = require('mongoose')
 
-const connectDB= async ()=> {
+const connectDB = async () => {
     try {
-       //!Additional Properties no longer needed
-       const conn = await mongoose.connect
-       (process.env.MONGO_URI)
+        //! Additional properties no longer needed.
+        const conn = await mongoose.connect(process.env.MONGO_URI)
 
-       console.log(`MongoDB Connected: ${conn,connection.host}`)
-    }catch (err){
-console.error(err)
-process.exit(1)
+        console.log(`MongoDB COnnected: ${conn.connection.host}`)
+    } catch (err) {
+        console.error(err)
+        process.exit(1)
     }
 }
+
 module.exports = connectDB
