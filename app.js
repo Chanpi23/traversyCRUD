4a629d86e8f6a8fc9fe04cec79fc304c08eaf425
chanpi23 <chandrikad34@gmail.com>
2022-08-08 20:07:05 -0400

    minor change

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const connectDB = require('./config/db')
 
 //Load config
 
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path:'./config/config.env'})
 
 connectDB()
 
 const app = express()
+
 const PORT= process.env.PORT || 8500
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+app.listen(PORT, 
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
