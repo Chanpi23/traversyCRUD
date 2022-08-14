6698ab06ef62565c7623d7834af9d9a997a14e3e
chanpi23 <chandrikad34@gmail.com>
2022-08-13 20:41:58 -0400

    Added file

--- app.js
+++ app.js
@@ -11,12 +11,20 @@ dotenv.config({ path:'./config/config.env'})
 connectDB()
 
 const app = express()
-
+// Logging
  if(process.env.NODE_ENV === 'development'){
     app.use (morgan('dev'))
  }
  // Handlebars
- 
+ //! add the word. engine after exphbs
+ app.engine('.hbs',exphbs.engine (
+    { defaultLayout: 'main', extname: '.hbs'}))
+ app.set ( 'view engine','.hbs')
+
+
+ //Routes
+app.use('/', require ['./routes/index'])
+app.use('/dashboard', require ['./routes/index'])
 
 const PORT= process.env.PORT || 8500
 
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+//@desc Login/landing page
+//@route GET /
+router.get('/', (req,res) => {
+    res.render('login', {
+        layout: 'login',
+    })
+})
+
+//@desc Dashboard
+//@route GET /dashboard
+router.get('/dashboard', (req,res) => {
+    console.log(req.user)
+    res.render('dashboard')
+})
+
+module.exports = router
